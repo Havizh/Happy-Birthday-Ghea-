f85639bf92d574441e61243cdb0c0e7b0defa77a
Havizh <hafizhalbana20@gmail.com>
2024-11-22 22:54:11 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('open-message-btn').addEventListener('click', function() {
-    document.getElementById('birthday-message').style.display = 'block'; // Menampilkan surat
-    document.getElementById('birthday-audio').play(); // Memutar lagu
-    this.style.display = 'none'; // Sembunyikan tombol "Buka Surat"
+    document.getElementById('birthday-message').style.display = 'block'; // Menampilkan ucapan
+    document.getElementById('birthday-audio').play(); // Memutar lagu "Whut"
+    this.style.display = 'none'; // Menyembunyikan tombol "Buka Ucapan"
 });
 
 document.getElementById('close-message-btn').addEventListener('click', function() {
-    document.getElementById('birthday-message').style.display = 'none'; // Menyembunyikan surat
-    document.getElementById('open-message-btn').style.display = 'block'; // Menampilkan kembali tombol "Buka Surat"
-    document.getElementById('birthday-audio').pause(); // Menjeda lagu
+    document.getElementById('birthday-message').style.display = 'none'; // Menyembunyikan ucapan
+    document.getElementById('open-message-btn').style.display = 'block'; // Menampilkan tombol "Buka Ucapan"
+    document.getElementById('').pause(); // Menjeda lagu
     document.getElementById('birthday-audio').currentTime = 0; // Mengulang lagu dari awal
 });
